b28276f4643d9280c52e995029ca27ddb2dde516
Ian Zimmerman <itz@buug.org>
2011-01-23 15:07:11 -0800

    more boring license stuff

--- aectl.sh
+++ aectl.sh
@@ -1,3 +1,9 @@
+# Copyright (C) 2011 Ian Zimmerman <itz@buug.org>
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the conditions spelled out in
+# the file LICENSE are met.
+
     #define a function to save current alsa equalizer state
     aes() 
     {
